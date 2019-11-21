419518f76cc3403de8fb38ada05c3e3b26b2489c
Panupong <5635512110@psu.ac.th>
2019-11-22 06:21:24 +0700

    first commit

---
+++ 1.helloworld.rb
@@ -0,0 +1,18 @@
+print "Hello World\n"
+puts "Hello World"
+p "Hello World"
+
+greeting = "Hello Panupong"
+puts greeting
+
+def sayHello
+    puts "Hello World"
+end
+
+def sayMessage(message)
+    puts message
+end
+
+sayMessage('Jesus is King')
+sayMessage 'Ruby is Great'
+sayHello
